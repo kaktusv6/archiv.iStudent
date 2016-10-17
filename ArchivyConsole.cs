a2195afc9713303307406517e717bddffe6dc4fe
Vasiliy <kaktusv6@gmail.com>
2016-10-17 15:15:11 +1100

    Удалил старый код.

--- ArchivyConsole.cs
+++ ArchivyConsole.cs
@@ -16,69 +16,7 @@ namespace ArchivyConsole
     {
 	    static void Main(string[] args)
         {
-			if (args.Length == 0 || args.Length == 1)
-			{
-				Console.WriteLine("Введите два аргумента:");
-				Console.WriteLine("1. <mode>	Ражим архиватора:	--com для сжатия файла");
-				Console.WriteLine("									--uncom для декодирования файла");
-				Console.WriteLine("2. [files, ] Имена файлов которые вы хотите сжать или декодировать");
-			}
-			else
-			{
-				string mode = args[0];
-				if (mode == "--com")
-				{
-					for (int i = 1; i < args.Length; i++)
-					{
-						// Запоминаем имя файла которое передается как параметр при запуске программы
-						string nameFile = args[i];
 
-						// Создание файлого потока для считывания данных
-						FileStream fileRead = File.OpenRead(nameFile);
-						StreamReader reader = new StreamReader(fileRead);
-
-						// Создание файлого потока для записи
-						FileStream fileWrite = File.OpenWrite(nameFile + ".sz");
-						// Создаем SnappyStream для сжатия данных при взятие данных из потока
-						SnappyStream compressor = new SnappyStream(fileWrite, CompressionMode.Compress);
-						// Создаем поток который запишет сжатые файлы и сразу передаем данный из считываемого файла
-						using (var writer = new StreamWriter(compressor))
-						{
-							writer.Write(reader.ReadToEnd());
-							writer.Close();
-						}
-						fileWrite.Close();
-						fileRead.Close();
-						reader.Close();
-					}
-				}
-				else if (mode == "--uncom")
-				{
-					for (int i = 1; i < args.Length; i++)
-					{
-						// Запоминаем имя файла которое передается как параметр при запуске программы
-						string nameFile = args[i];
-
-						// Создание файлого потока для считывания данных
-						FileStream fileRead = File.OpenRead(nameFile);
-
-						// Создаем SnappyStream для декодирования данных при взятие данных из потока
-						SnappyStream decompressor = new SnappyStream(fileRead, CompressionMode.Decompress);
-						// Создаем поток который считает сжатые файлы и сразу передаем данные из считываемого файла
-						StreamReader reader = new StreamReader(decompressor);
-						// Создание файлого потока для записи
-						FileStream fileWrite = File.OpenWrite(nameFile.Substring(0, nameFile.Length - 3));
-						using (var writer = new StreamWriter(fileWrite))
-						{
-							writer.Write(reader.ReadToEnd());
-							writer.Close();
-						}
-						reader.Close();
-						fileRead.Close();
-						fileWrite.Close();
-					}
-				}
-			}
         }
     }
 }
