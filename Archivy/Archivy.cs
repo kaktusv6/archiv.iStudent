0cdf2bdcdddbfbd30771aa380a23f6e0c7c378ed
Vasiliy <kaktusv6@gmail.com>
2016-10-17 17:57:50 +1100

    Добавил класс Archivy.

---
+++ Archivy/Archivy.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.IO.Compression;
+
+// Подключение библиотекаи Snappy
+using Snappy;
+
+namespace ConsoleApplicationArchivy
+{
+	static class Archivy
+	{
+		static public void Compress(string nameFile)
+		{
+			string pathToFile = Path.GetFullPath(nameFile);
+			FileInfo fi = new FileInfo(pathToFile);
+
+			using (FileStream fs = fi.OpenRead())
+			{
+				using (FileStream outFile1 = File.Create(nameFile + ".sz"))
+				{
+					using (SnappyStream compress = new SnappyStream(outFile1, CompressionMode.Compress))
+					{
+						fs.CopyTo(compress);
+					}
+				}
+			}
+		}
+
+		static public void Decompress(string nameFile)
+		{
+			string pathToFile = Path.GetFullPath(nameFile);
+
+			FileInfo fi = new FileInfo(pathToFile);
+
+			using (FileStream inFile = fi.OpenRead())
+			{
+				string curFile = fi.FullName;
+				string origName = curFile.Remove(curFile.Length -
+						fi.Extension.Length);
+
+				using (FileStream outFile = File.Create(origName))
+				{
+					using (SnappyStream decompress = new SnappyStream(inFile, CompressionMode.Decompress))
+					{
+						decompress.CopyTo(outFile);
+					}
+				}
+			}
+		}
+	}
+}
